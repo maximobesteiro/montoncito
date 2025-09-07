0450ebce70ed1af39a025e7fa2b6458af0b13bb0
Máximo Besteiro <machibest@gmail.com>
2025-09-07 18:47:19 +0200

    Renamed test folder for game-core package

--- packages/core-game/vitest.config.ts
+++ packages/core-game/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["tests/**/*.test.ts"],
+    include: ["test/**/*.test.ts"],
     environment: "node",
     coverage: {
       provider: "v8", // or "istanbul" if you prefer
